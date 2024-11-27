63a152022ed2765a4976771813c3bb044dc6faaa
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-11-27 23:05:27 +0900

    feat: create the generation function

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
 
-func main() {
-	fmt.Println("hello, welcome to the first project using nvim")
+type URLMapping struct {
+	ID          int
+	OriginalURL string
+	ShortCode   string
+}
+
+func generateShortCode() (string, error) {
+	b := make([]byte, 6)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b)[:6], nil
 }
