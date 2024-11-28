5614b210b76fdff0e81b10a427587218c96d9411
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-11-28 23:30:25 +0900

    feat: db initial function

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rand"
+	"database/sql"
 	"encoding/base64"
 )
 
@@ -17,4 +18,26 @@ func generateShortCode() (string, error) {
 		return "", err
 	}
 	return base64.URLEncoding.EncodeToString(b)[:6], nil
+}
+
+const (
+	dbConnStr = ""
+)
+
+func initDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", dbConnStr)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS url_mappings (
+			id SERIAL PRIMARY KEY
+			original_url TEXT NOT NUL
+			short_code VARCHAR(10) NOT NULL UNIQUE
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)
+		`)
+
+	return db, err
 }
